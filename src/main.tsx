ccd2c742308f40ec33a2d1b5fe8b044d7e3f9abb
ahmeterenlacinbala <ahmetlacinbala@gmail.com>
2025-05-13 01:17:57 +0300

    ghpages config

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Flashcard from './components/Flashcard.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.PROD ? '/kanji-agent-front' : '/'}>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/flashcards" element={<Flashcard />} />
